9dc1473b82cec14af185c3561f038e638bd1cb2e
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-11 15:40:06 +0500

    Subcription Duration period

--- app/SubscriptionDuration.php
+++ app/SubscriptionDuration.php
@@ -19,4 +19,34 @@ class SubscriptionDuration extends Model
     {
         return $this->belongsTo(Cheat::class);
     }
+
+    public function getPeriod()
+    {
+        $duration = $this->duration;
+        if ($duration < 24) {
+            $postfix = "часов";
+            if ($duration == 1)
+                $postfix = "час";
+            else if ($duration >= 2 and $duration <= 4)
+                $postfix = "часа";
+            return "$duration $postfix";
+        }
+        else if ($duration >= 720) {
+            $postfix = "месяцев";
+            $months = $duration / 720;
+            if ($months == 1)
+                $postfix = "месяц";
+            else if ($months >= 2 and $months <= 4)
+                $postfix = "месяца";
+            return "$months $postfix";
+        } else {
+            $postfix = "месяцев";
+            $days = $duration / 24;
+            if ($days == 1)
+                $postfix = "день";
+            else if ($days >= 2 and $days <= 4)
+                $postfix = "дня";
+            return "$days $postfix";
+        }
+    }
 }
