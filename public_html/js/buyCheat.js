7b0a3620125c38757c66b349c12c860f77f827b9
dmitriyl99 <dmitriyl1899@gmail.com>
2020-08-12 13:30:01 +0500

    Try to fix scripts

--- public_html/js/buyCheat.js
+++ public_html/js/buyCheat.js
@@ -1,6 +1,6 @@
 $(function () {
     $('#customRange2').on('change', function () {
-        let index = this.value;
+        let index = this.value - 1;
         let price = $(`#durationPrice${index}`).html();
         $('#payButton').html(`Перейти к оплате (${price} руб.)`)
     });
